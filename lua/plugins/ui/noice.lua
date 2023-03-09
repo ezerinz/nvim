ad797bcc3de6f5a3900da872105371a5456850aa
ezerinz <edwinnnzx@gmail.com>
2023-03-09 21:33:38 +0800

    update

--- lua/plugins/ui/noice.lua
+++ lua/plugins/ui/noice.lua
@@ -1,12 +1,21 @@
 return {
 	"folke/noice.nvim",
 	event = "VeryLazy",
-	enabled = true,
 	dependencies = {
 		"MunifTanjim/nui.nvim",
 		"rcarriga/nvim-notify",
 	},
 	opts = {
+		routes = {
+			{
+				filter = {
+					event = "msg_show",
+					kind = "",
+					find = "written",
+				},
+				opts = { skip = true },
+			},
+		},
 		lsp = {
 			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
 			override = {
